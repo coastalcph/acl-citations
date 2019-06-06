4eff6719514fa501fab539ac05c18081b275d62d
Marcel Bollmann <bollmann@linguistics.rub.de>
2019-06-06 21:17:46 +0200

    Add scripts and instructions

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# acl-citations
-Analyzing what people cite in the ACL Anthology
+# ACL Citations
+
+## Prerequisites
+
+1. Install requirements for Python scripts in this repo:
+
+   `pip install -r requirements.txt`
+
+2. Install GROBID.  The easiest way is via docker:
+
+   `docker pull lfoppiano/grobid:0.5.4_1`
+
+3. Get the following repository as well (should have no additional dependencies):
+
+   `git clone https://github.com/kermitt2/grobid-client-python`
+
+
+## Running the pipeline
+
+1. Download the desired PDF files from the Anthology by specifying their IDs.
+   Wildcards `?` and `*` are allowed.  The following would fetch all long and
+   short papers from NAACL 2018:
+
+   `python ./acl_anthology.py fetch 'N18-1*' 'N18-2*'`
+
+   The downloaded files will be in a subfolder `pdf/N18/`.
+
+2. Start the GROBID server (if it's not already running).  Best to do this in a
+   separate terminal:
+
+   `docker run -t --rm --init -p 8080:8070 -p 8081:8071 lfoppiano/grobid:0.5.4_1`
+
+3. Run the PDFs through GROBID:
+
+   `mkdir -p tei/N18`
+   `python grobid-client-python/grobid-client.py --input pdf/N18 --output tei/N18 --config config.json processReferences`
+
+4. Extract the citation years from the TEI files:
+
+   `python ./parse_tei.py tei/N18/* --csv N18.csv`
+
+`N18.csv` will be a tab-separated file containing the paper ID in the first
+column, paper published date in the second column, and a comma-separated list of
+years of cited papers in the third column.
---
+++ acl_anthology.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+
+"""
+Download files from the ACL Anthology.
+
+Usage:
+  acl_anthology.py -h
+  acl_anthology.py update
+  acl_anthology.py (match|fetch) <expr>... [options]
+
+Arguments:
+  <expr>                   Expression specifying an ACL Anthology ID;
+                           may contain ? and * wildcards.
+
+Options:
+  --debug                  Verbose log messages.
+  -h, --help               Display this helpful text.
+"""
+
+from docopt import docopt
+import better_exceptions
+from glob import glob
+import logging
+import logzero
+from logzero import logger as log
+from lxml import etree
+import os
+import re
+import requests
+import time
+from tqdm import tqdm
+
+
+ACL_REPO = "https://github.com/acl-org/acl-anthology"
+ANTHOLOGY_URL = "https://www.aclweb.org/anthology/{}"
+SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def update_acl_repo(repo_dir, force=False):
+    from datetime import datetime, timedelta
+    from git import Repo
+    repo_token = os.path.join(repo_dir, ".pulled")
+
+    if not os.path.exists(repo_dir):
+        os.mkdir(repo_dir)
+        log.info("Fetching Anthology metadata...")
+        repo = Repo.clone_from(ACL_REPO, repo_dir, branch="master")
+        with open(repo_token, 'a'):
+            os.utime(repo_token, None)
+        delta = timedelta(hours=0)
+    elif os.path.exists(repo_token):
+        delta = datetime.now() - datetime.fromtimestamp(
+            os.path.getmtime(repo_token)
+        )
+        log.debug("Last checked for metadata updates: {} ago".format(":".join(str(delta).split(":")[:2])))
+    else:
+        delta = timedelta.max
+
+    repo = Repo(repo_dir)
+    if force or delta > timedelta(hours=24):
+        log.info("Checking Anthology metadata for updates...")
+        repo.remotes.origin.pull()
+        with open(repo_token, 'a'):
+            os.utime(repo_token, None)
+    else:
+        log.info("Anthology metadata is up-to-date.")
+
+
+def match_ids(ids):
+    map_to_prefix = lambda x: x[:x.find("*")+1] if "*" in x[:3] else x[:3]
+    map_to_regex = lambda x: x.replace("?", ".").replace("*", ".+")
+
+    id_regex = "|".join(map_to_regex(x) for x in ids)
+    id_pattern = re.compile(id_regex)
+    file_regex = "|".join(map_to_regex(map_to_prefix(x)) for x in ids)
+    file_pattern = re.compile(file_regex)
+
+    matched = []
+    for xmlfile in glob(f"{SCRIPTDIR}/.anthology-repo/data/xml/*.xml"):
+        prefix = os.path.basename(xmlfile)[:3]
+        if file_pattern.match(prefix) is None:
+            continue
+        log.debug(f"Parsing file: {xmlfile}")
+        tree = etree.parse(xmlfile)
+        for paper in tree.getroot():
+            full_id = f"{prefix}-{paper.get('id')}"
+            if id_pattern.match(full_id) is None:
+                continue
+            log.debug(f"Matched: {full_id}")
+            if "href" in paper.attrib:
+                url = paper.get("href")
+            else:
+                url = ANTHOLOGY_URL.format(full_id)
+            matched.append((full_id, url))
+
+    log.info(f"Found {len(matched)} matching {'entry' if len(matched)==1 else 'entries'}.")
+    return matched
+
+
+def check_ids(ids, force=False):
+    checked = []
+    for full_id, url in ids:
+        local_file = f"{SCRIPTDIR}/pdf/{full_id[:3]}/{full_id}.pdf"
+        if not force and os.path.exists(local_file):
+            continue
+        if not os.path.exists(os.path.dirname(local_file)):
+            os.makedirs(os.path.dirname(local_file))
+        checked.append((full_id, url, local_file))
+
+    if len(checked) < len(ids):
+        log.info(f"Skipping download of {len(ids) - len(checked)} files.")
+
+    return checked
+
+
+def download_ids(ids):
+    log.info(f"Downloading {len(entries)} {'file' if len(entries)==1 else 'files'}...")
+    progress = tqdm(total=len(entries), unit="files")
+    for i, (full_id, url, local_file) in enumerate(entries):
+        progress.set_description_str(f"{full_id} ")
+        h = requests.head(url, allow_redirects=True)
+        content_type = h.headers.get('content-type')
+        if 'pdf' not in content_type.lower():
+            progress.write(f"{url} is not a PDF file")
+            progress.update()
+            continue
+        r = requests.get(url, allow_redirects=True)
+        with open(local_file, 'wb') as f:
+            f.write(r.content)
+        progress.update()
+        if (i+1) % 50 == 0:
+            tqdm.write(f"Downloaded {i+1:4d} files -- pausing for 10 seconds")
+            time.sleep(10)
+    progress.close()
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+
+    log_level = logging.DEBUG if args["--debug"] else logging.INFO
+    logzero.loglevel(log_level)
+    logzero.formatter(logzero.LogFormatter(datefmt="%Y-%m-%d %H:%M:%S"))
+
+    update_acl_repo(f"{SCRIPTDIR}/.anthology-repo", force=args["update"])
+    if args["match"] or args["fetch"]:
+        entries = match_ids(args["<expr>"])
+    if args["fetch"]:
+        entries = check_ids(entries)
+        if entries:
+            download_ids(entries)
